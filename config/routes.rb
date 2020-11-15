791dd1c32576e326e146cde45038e1662a4ef43a
Quentin De Malherbe <quentdemalherbe@gmail.com>
2020-11-15 17:37:36 +0100

    style un peu

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
     resources :ingredients, only: [:show]
     end
   end
-  # get 'liste/:id/ingredients', to: 'listes#show_ingredients', as: 'liste_ingredients'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
